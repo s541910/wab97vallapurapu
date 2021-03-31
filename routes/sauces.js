922ce96fbbc8dff3a0ea42dce1c92fa9903f626d
s541910 <77760915+s541910@users.noreply.github.com>
2021-03-31 14:03:52 -0500

    added classes sauces

--- routes/sauces.js
+++ routes/sauces.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.send('sauces',{title: 'search Results of Sauces'});  
+    res.render('sauces',{title: 'search Results of Sauces'});  
  });
 module.exports = router;
